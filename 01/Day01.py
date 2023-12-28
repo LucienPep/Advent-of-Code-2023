0d23ac87c2517ccc1bbf5991f64d1b1a2a3faf81
LucienPep <xxyyzzoozz@gmail.com>
2023-12-28 16:09:28 +0000

    Started part 2 but data is incorrect

--- 01/Day01.py
+++ 01/Day01.py
@@ -13,14 +13,31 @@ for line in calibration:
             lineNumbers += value
     calibrationTotal += int(lineNumbers[0] + lineNumbers[-1])
 
-print(calibrationTotal)
+#print(calibrationTotal)
 
 #Part 02
+calibrationTotal = 0
+numberSet = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+numberDict = {'one': '1','two': '2','three': '3','four': '4','five': '5','six': '6','seven': '7','eight': '8','nine': '9','zero': '0'}
+stringSet = ''
 
 for line in calibration:
     lineNumbers = ''
-    if line 
+    stringSet = ''
+
     for value in line:
         if value.isdigit():
             lineNumbers += value
-        elif 
+        else:
+            stringSet += value
+            for number in numberSet:
+                if number in stringSet:
+                    lineNumbers += (numberDict[number])
+                    stringSet = ''
+    
+    print(lineNumbers)
+    calibrationTotal += int(lineNumbers[0] + lineNumbers[-1])
+
+#print(calibrationTotal)
+    
+#Line 22 might pose a problem :/
